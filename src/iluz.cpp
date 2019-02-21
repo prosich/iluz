f325d0bea1295784f7afd5ef11764f9961f1e45d
prosich <prosich@gmail.com>
2019-02-21 14:03:45 +0100

    con interrupcion

--- src/iluz.cpp
+++ src/iluz.cpp
@@ -3,14 +3,14 @@
 #include "config.h"
 #include "ver.h"
 
-//const char* mqtt_server = "broker.hivemq.com";
-//const int mqtt_port = 1883;
-const char* mqtt_server = "m21.cloudmqtt.com";
-const int mqtt_port = 19645;
-const char* mqtt_user = "lzxaksqt";
-const char* mqtt_pass = "MEsnDE8fOjpl";
-Espini *cosa;
+const byte DREAD = D2;
 
+const char* mqtt_server = "m21.cloudmqtt.com";
+const int   mqtt_port   = 19645;
+const char* mqtt_user   = "lzxaksqt";
+const char* mqtt_pass   = "MEsnDE8fOjpl";
+
+Espini *cosa;
 WiFiClient espClient;
 PubSubClient *mqttc;
 
@@ -18,25 +18,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
     cosa->log("Callback!");
 }
 
+void flip() {
+    cosa->log(String(digitalRead(DREAD)).c_str());
+    mqttc->publish("iluz", String(digitalRead(DREAD)).c_str());
+}
+
 void setup() {
   cosa=new Espini(wifis(),"iluz",ver,otasrv,logsrv);
-  Serial.println("Conectado");
   cosa->log("Iniciando");
+
   mqttc=new PubSubClient(espClient);
   mqttc->setServer(mqtt_server,mqtt_port);
   mqttc->setCallback(callback);
-  cosa->log("entrando en bucle de conexion");
-  while (!mqttc->connected()) {
-    cosa->log("conectaNdo");
-    if (mqttc->connect(cosa->getchipid(),mqtt_user, mqtt_pass)) {
-      cosa->log("conectado!");
-      }else{
-      cosa->log("reintentando, rc="); //cosa->log(mqttc->state());
-    }
-  }
+  cosa->log("Conectando");
+  while (!mqttc->connect(cosa->getchipid(),mqtt_user, mqtt_pass)) delay(5000);
+  cosa->log("Conectado!");
+
+  pinMode(DREAD, INPUT);
+  attachInterrupt(digitalPinToInterrupt(DREAD), flip, CHANGE);
 }
 
 void loop() {
-  cosa->log("LOOP!!"); delay(10000);
-  mqttc->publish("luz", "hello world");
+  while (true) { cosa->log("ALIVE!!"); delay(300e3); }
 }
+
